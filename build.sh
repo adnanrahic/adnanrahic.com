fcbf33954cc0aa4706fbfe1925cfbcdfea8229aa
Adnan Rahic <ado.raha198@gmail.com>
2022-02-20 12:37:51 +0100

    push public dir

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 hugo
 find public -type f -name '*.html' -exec $(npm bin)/html-minifier {} -c htmlmin.json -o {}.out \; -exec mv {}.out {} \;
 find public/js -type f -name '*.js' -exec $(npm bin)/babel {} -o {}.out \; -exec mv {}.out {} \;
-sudo apt-get install jpegoptim
-sudo apt-get install optipng
+# sudo apt-get install jpegoptim
+# sudo apt-get install optipng
 cd public/img
-jpegoptim *.jpg
+# jpegoptim *.jpg
 # optipng *.png
 cd ../..
 cp -R zohoverify ./public/
---
+++ public/sw.js
@@ -0,0 +1,10 @@
+/* global UpUp */
+UpUp.start({
+  'content-url': 'index.html',
+  'assets': [
+    'js/jquery.min.js',
+    'js/bootstrap.min.js',
+    'js/img-load.js',
+    'img/home.jpg'
+  ]
+})
